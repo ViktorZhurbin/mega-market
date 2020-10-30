47700dec782a4961e411cc5c8792fd4fabda2041
Viktor Zhurbin <vzhurbin@gmail.com>
2020-10-30 18:32:17 +0300

    update Auth

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -3,7 +3,11 @@ import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './Auth.module.css';
 
 export const Auth: React.FC = () => {
-    const [session] = useSession();
+    const [session, loading] = useSession();
+
+    if (loading) {
+        return <span>Loading session...</span>;
+    }
 
     return !session ? (
         <button onClick={() => signIn()}>Sign in</button>
