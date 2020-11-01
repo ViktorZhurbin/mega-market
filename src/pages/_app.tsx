157eeead5322945b6a254ced32b876798ce869c7
Viktor Zhurbin <vzhurbin@gmail.com>
2020-11-01 03:40:50 +0300

    custom _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,13 @@
 import React, { ReactElement } from 'react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import { Provider } from 'next-auth/client';
 
 import { AppContextProvider } from '../contexts';
-import { typography } from '../styles/typography';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
     return (
         <>
-            <Head>
-                <style
-                    id={'typography.js'}
-                    dangerouslySetInnerHTML={{
-                        __html: typography.toString(),
-                    }}
-                />
-            </Head>
-
             <Provider session={pageProps.session}>
                 <AppContextProvider>
                     <Component {...pageProps} />
