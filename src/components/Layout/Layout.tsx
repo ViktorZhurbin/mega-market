e00fd229721e83f4bcdb3ef47b658ab52876393b
Viktor Zhurbin <vzhurbin@gmail.com>
2020-11-01 03:54:27 +0300

    update styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Auth } from '../Auth';
 import { Nav } from '../Nav';
 
+import styles from './Layout.module.css';
+
 export const Layout: React.FC = ({ children }) => {
     return (
-        <div>
+        <div className={styles.container}>
             <Auth />
             {children}
             <Nav />
