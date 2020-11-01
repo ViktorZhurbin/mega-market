a8118bf14a2f42144ebae75eead29730604fc31e
Viktor Zhurbin <vzhurbin@gmail.com>
2020-11-01 04:06:51 +0300

    fix styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,9 +5,11 @@ import styles from './Layout.module.css';
 
 export const Layout: React.FC = ({ children }) => {
     return (
-        <div className={styles.container}>
-            <Auth />
-            {children}
+        <div className={styles.wrapper}>
+            <div className={styles.content}>
+                <Auth />
+                {children}
+            </div>
             <Nav />
         </div>
     );
